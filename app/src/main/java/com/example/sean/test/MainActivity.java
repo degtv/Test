b1d652272e23cc1eb1e662fa69455602ffaa5a6e
lonereflection <sean870620@yahoo.com.tw>
2017-01-23 17:12:05 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sean/test/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.sean.test;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Intent intent;
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent.setClass(MainActivity.this  , page2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
